1d67d8fba5847b87b118ae01bcfa0050b40f47fb
Роман Зопиров <irovbyte@outlook.com>
2025-12-07 02:23:01 +0500

    2

--- cs/obj/Debug/net8.0/cs.AssemblyInfo.cs
+++ cs/obj/Debug/net8.0/cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914614ed976a4a84bcb454a3b7cff782601df89b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd92d143553a5346bd75a29e2467a1776dc0434")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
