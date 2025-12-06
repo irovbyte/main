914614ed976a4a84bcb454a3b7cff782601df89b
irovbyte <irovbyte@outlook.com>
2025-12-06 15:50:48 +0500

    1

---
+++ cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int like(int a, int b) { return a + b; }
+
+int main(void) {
+
+  int x, y;
+  cin >> x >> y;
+  int z = like(x, y);
+  cout << z;
+  return 0;
+}
---
+++ cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number;
+            number = 100 + 200; //300
+            Console.WriteLine(number);
+            number = number + 50; //350
+            Console.WriteLine(number);
+            Console.WriteLine();
+        }
+    }
+}
