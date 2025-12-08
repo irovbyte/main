30d2a2d5977ec2ba31f0eeabbd860b9c4667e0c1
Роман Зопиров <irovbyte@outlook.com>
2025-12-08 07:43:43 +0500

    5

--- cs/Program.cs
+++ cs/Program.cs
@@ -2,17 +2,17 @@
 
 namespace cs
 {
-class Program
-{
-    static void Main()
+    class Program
     {
-        int number;
-        float num = 213.62356023478153f;
-        number = 100 + 200; // 300
-        Console.WriteLine("Peremennaya: " + number + ".");
-        number = number + 50; // 350
-        Console.WriteLine(number);
-        Console.WriteLine(num);
+        static void Main()
+        {
+            int number;
+            float num = 213.62356023478153f;
+            number = 100 + 200; //300
+            Console.WriteLine("Peremennaya: " + number + ".");
+            number = number + 50; //350
+            Console.WriteLine(number);
+            Console.WriteLine(num);
+        }
     }
-}
 }
